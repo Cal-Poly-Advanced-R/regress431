7f97c59638b6961b8228d89e14aa6bb10e4e972d
Kelly N. Bodwin <kbodwin@calpoly.edu>
2020-05-10 17:23:41 -0700

    Fix double assignment typo

--- tests/testthat/test-ridge_regression.R
+++ tests/testthat/test-ridge_regression.R
@@ -4,7 +4,7 @@ test_that("ridge regression correctly calculates coefficients", {
     dplyr::select(mpg, hp, cyl) %>%
     ridge_regression(mpg, lambda = 0.5)
 
-  mtcars2 <- mtcars2 <- mtcars %>%
+  mtcars2 <- mtcars %>%
     dplyr::select(mpg, cyl, hp) %>%
     dplyr::mutate(
       cyl = scale(cyl),
@@ -18,7 +18,7 @@ test_that("ridge regression correctly calculates coefficients", {
   expect_equal(coef(mass_result)[['cyl']], my_result$cyl,
                tolerance = 0.05, scale = abs(my_result$cyl))
 
-  expect_equal(coef(mass_result)[1], my_result$Intercept,
+  expect_equal(coef(mass_result)[[1]], my_result$Intercept,
                tolerance = 0.05, scale = abs(my_result$Intercept))
 })
 
